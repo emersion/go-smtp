b324b20167b719f06400dd6e707e88db60d8db81
emersion <contact@emersion.fr>
2017-08-22 14:44:31 +0200

    Add Server.ForEachConn

--- server.go
+++ server.go
@@ -176,3 +176,12 @@ func (s *Server) Close() {
 func (s *Server) EnableAuth(name string, f SaslServerFactory) {
 	s.auths[name] = f
 }
+
+// ForEachConn iterates through all opened connections.
+func (s *Server) ForEachConn(f func(*Conn)) {
+	s.locker.Lock()
+	defer s.locker.Unlock()
+	for conn := range s.conns {
+		f(conn)
+	}
+}
