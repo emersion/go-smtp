51a34b97a76802282d9399fa43e26f555ac43c59
emersion <contact@emersion.fr>
2018-09-17 16:59:52 +0200

    Fix HELO/EHLO not uppercased

--- conn.go
+++ conn.go
@@ -80,7 +80,8 @@ func (c *Conn) handle(cmd string, arg string) {
 		return
 	}
 
-	switch strings.ToUpper(cmd) {
+	cmd = strings.ToUpper(cmd)
+	switch cmd {
 	case "SEND", "SOML", "SAML", "EXPN", "HELP", "TURN":
 		// These commands are not implemented in any state
 		c.WriteResponse(502, fmt.Sprintf("%v command not implemented", cmd))
