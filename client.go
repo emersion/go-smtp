5abcd81dba5fb6e2655ec9bee6dedc622f58aa37
Simon Ser <contact@emersion.fr>
2020-07-07 14:44:36 +0200

    client: remove mention of net/smtp

--- client.go
+++ client.go
@@ -456,11 +456,10 @@ var testHookStartTLS func(*tls.Config) // nil, except for tests
 // messages is accomplished by including an email address in the to
 // parameter but not including it in the r headers.
 //
-// The SendMail function and the net/smtp package are low-level
-// mechanisms and provide no support for DKIM signing, MIME
-// attachments (see the mime/multipart package), or other mail
-// functionality. Higher-level packages exist outside of the standard
-// library.
+// The SendMail function and the go-smtp package are low-level
+// mechanisms and provide no support for DKIM signing (see go-msgauth), MIME
+// attachments (see the mime/multipart package or the go-message package), or
+// other mail functionality.
 func SendMail(addr string, a sasl.Client, from string, to []string, r io.Reader) error {
 	if err := validateLine(from); err != nil {
 		return err
