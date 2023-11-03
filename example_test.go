8d509288b394b6b234b5934e9cbbe7eb498ea421
Simon Ser <contact@emersion.fr>
2023-11-03 10:55:00 +0100

    Split server example into separate file
    This makes GoDoc show the full file as example code.

--- example_test.go
+++ example_test.go
@@ -5,13 +5,9 @@
 package smtp_test
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"strings"
-	"time"
 
 	"github.com/emersion/go-sasl"
 	"github.com/emersion/go-smtp"
@@ -85,69 +81,6 @@ func ExampleSendMail() {
 		"This is the email body.\r\n")
 	err := smtp.SendMail("mail.example.com:25", auth, "sender@example.org", to, msg)
 	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-// The Backend implements SMTP server methods.
-type Backend struct{}
-
-// NewSession is called after client greeting (EHLO, HELO).
-func (bkd *Backend) NewSession(c *smtp.Conn) (smtp.Session, error) {
-	return &Session{}, nil
-}
-
-// A Session is returned after successful login.
-type Session struct{}
-
-// AuthPlain implements authentication using SASL PLAIN.
-func (s *Session) AuthPlain(username, password string) error {
-	if username != "username" || password != "password" {
-		return errors.New("Invalid username or password")
-	}
-	return nil
-}
-
-func (s *Session) Mail(from string, opts *smtp.MailOptions) error {
-	log.Println("Mail from:", from)
-	return nil
-}
-
-func (s *Session) Rcpt(to string, opts *smtp.RcptOptions) error {
-	log.Println("Rcpt to:", to)
-	return nil
-}
-
-func (s *Session) Data(r io.Reader) error {
-	if b, err := ioutil.ReadAll(r); err != nil {
-		return err
-	} else {
-		log.Println("Data:", string(b))
-	}
-	return nil
-}
-
-func (s *Session) Reset() {}
-
-func (s *Session) Logout() error {
-	return nil
-}
-
-func ExampleNewServer() {
-	be := &Backend{}
-
-	s := smtp.NewServer(be)
-
-	s.Addr = ":1025"
-	s.Domain = "localhost"
-	s.WriteTimeout = 10 * time.Second
-	s.ReadTimeout = 10 * time.Second
-	s.MaxMessageBytes = 1024 * 1024
-	s.MaxRecipients = 50
-	s.AllowInsecureAuth = true
-
-	log.Println("Starting server at", s.Addr)
-	if err := s.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
 }
